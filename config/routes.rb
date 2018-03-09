8fd1ea0988e54de56a383df6320fa7c11c3d757f
kentcrozier <kcrozier204@outlook.com>
2018-03-09 12:44:44 -0600

    Add controller and view for Tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'tags', to: 'tags#index', as: 'tags'
+  get 'tags/:id', to: 'tags#show', as: 'tag'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
